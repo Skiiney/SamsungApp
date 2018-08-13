fc35c944dbdcd1f013331d7e8ff8efe6330ead4f
victor <25647735+dotsomething@users.noreply.github.com>
2018-08-13 20:03:15 -0300

    Update msgRoutes.js

--- api/routes/msgRoutes.js
+++ api/routes/msgRoutes.js
@@ -1,23 +1,16 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = (app) => {
 
-var messages = require('../controllers/msgController');
+const messages = require('../controllers/msgController');
 
 // messages Routes
-
 app.route('/messages')
-
    .get(messages.list_all_messages)
-
    .post(messages.create_a_message);
 
 app.route('/messages/:msgId')
-
    .get(messages.read_a_message)
-
    .put(messages.update_a_message)
-
    .delete(messages.delete_a_message);
-
 };
