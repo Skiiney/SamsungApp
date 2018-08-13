56bf3314c19353b99a2bd8b4ae95b87cde2d0972
victor <25647735+dotsomething@users.noreply.github.com>
2018-08-13 20:04:05 -0300

    Update msgModel.js

--- api/models/msgModel.js
+++ api/models/msgModel.js
@@ -1,30 +1,20 @@
 'use strict';
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-var msgSchema = new Schema({
-
+const msgSchema = new Schema({
    title: {
-
       type: String
    },
-
    body: {
-
       type: String
-
    },
-
    Created_date: {
-
       type: Date,
-
       default: Date.now
-
    }
-
 });
 
 module.exports = mongoose.model('Messages', msgSchema);
