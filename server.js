4e9865f261149c7f45b04210654c0131ac51d6ae
victor <25647735+dotsomething@users.noreply.github.com>
2018-08-13 20:02:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-/*
-var express = require('express'),
-
-app = express(),
-
-port = process.env.PORT || 3000;
-
-app.listen(port);
-
-app.get('/', function(req, res) { res.json({hello : 'world'});})
-
-console.log('Message RESTful API server started on: ' + port);
-
-*/
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
