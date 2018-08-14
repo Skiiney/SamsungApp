ef1bb3c09cc7cd9225e2ede958cf07a8779ed9ec
Skiiney <vihtorg@gmail.com>
2018-08-14 20:50:36 -0300

    Vamos ver

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -4,21 +4,7 @@ const mongoose = require("mongoose");
 const userSchema =  mongoose.Schema({
   name: {
     type: String
-  },
-  lasname: {
-    type: String
-  },
-  email: {
-    type: String,
-    unique: true
-  },
-  password: {
-    type: String
-  },
-  Created_date: {
-    type: Date,
-    default: Date.now
   }
-});
+  });
 
 module.exports = mongoose.model("user", userSchema);
