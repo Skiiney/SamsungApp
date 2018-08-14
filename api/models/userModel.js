55645a9c4df27655cf679d3a06d6a1ce1b332d76
victor <freirel.victor@gmail.com>
2018-08-14 20:46:56 -0300

    algumas mudancas

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,9 +1,7 @@
 "use strict";
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
+const userSchema =  mongoose.Schema({
   name: {
     type: String
   },
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 
 const User = require('./api/models/userModel');
