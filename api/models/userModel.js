ced3b96d91616d68b08278e1eb7b60e926a2d1b9
victor <freirel.victor@gmail.com>
2018-08-14 21:22:24 -0300

    algumas mudancas

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,21 +1,15 @@
 "use strict";
 const mongoose = require("mongoose");
 
-const userSchema =  mongoose.Schema({
-  name: {
-    type: String
-  },
-  lasname: {
-    type: String
-  },
+const userSchema = mongoose.Schema({
+  name: String,
+  lasname: String,
   email: {
     type: String,
     unique: true
   },
-  password: {
-    type: String
-  },
-  timestamps: true
+  password: String,
+  time: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("user", userSchema);
