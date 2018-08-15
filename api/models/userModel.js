daa48f19d502237023d43b252af98da6f384d9ab
victor <freirel.victor@gmail.com>
2018-08-14 21:39:43 -0300

    algumas mudancas

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -9,7 +9,8 @@ const userSchema = mongoose.Schema({
     unique: true
   },
   password: String,
-  timestamps: true
+  {
+    timestamps: true
 });
 
 module.exports = mongoose.model("user", userSchema);
